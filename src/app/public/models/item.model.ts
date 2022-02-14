150a2c7852454641d02ba0ef14bcbb5bcaa0d409
Conventional Changelog Action <conventional.changelog.action@github.com>
2022-02-14 11:28:43 +0000

    Auto-generated commit [ci skip]
    Triggered by commit: https://github.com/emirlemes/order-app/commit/3736323b903905953aef8e3bbb5d77482d30983b

--- src/app/public/models/item.model.ts
+++
@@ -1,9 +0,0 @@
-export class Item {
-  constructor(
-    public name: string,
-    public imageUrl: string,
-    public price: number,
-    public size: number,
-    public measuringUnit: string) { }
-
-}
--- src/environments/environment.prod.ts
+++
@@ -1,4 +0,0 @@
-export const environment = {
-  production: true,
-  api_key: 'AIzaSyD8TmAg22Ur02SbvBK0pNwnlDExT0e_AjE'
-}
