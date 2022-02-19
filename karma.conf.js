037097ecc7fb370ade37bce6f0a137d78372e462
Emir Lemeš <emir.lemes@gmail.com>
2022-02-20 00:04:15 +0100

    Name change

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/order-app'),
+      dir: require('path').join(__dirname, './coverage/orderAppWeb'),
       subdir: '.',
       reporters: [
         { type: 'html' },
@@ -40,5 +40,5 @@ module.exports = function (config) {
     browsers: ['Chrome'],
     singleRun: false,
     restartOnFileChange: true
-  });
-};
+  })
+}
